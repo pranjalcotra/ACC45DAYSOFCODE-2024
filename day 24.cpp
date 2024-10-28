f5b01cc755433169d82187f68439bcad3b0fd302
pranjalcotra <bansalpranjal80@gmail.com>
2024-10-28 17:36:38 +0530

    Day 24 - Single Number

---
+++ day 24.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int singleNumber(int* nums, int numsSize) {
+    int result = 0;
+    for (int i = 0; i < numsSize; i++) {
+        result ^= nums[i];
+    }
+    return result;
+}
+
+int main() {
+    int nums[] = {4, 1, 2, 1, 2};
+    int size = sizeof(nums) / sizeof(nums[0]);
+    int single = singleNumber(nums, size);
+    printf("The single number is: %d\n", single);
+    return 0;
+}
